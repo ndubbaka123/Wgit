ead0c960140b039307c0168a28500d8249af84a0
ndubbaka123 <65750357+ndubbaka123@users.noreply.github.com>
2020-05-23 01:48:25 -0400

    changes

--- Wgit/Program.cs
+++ Wgit/Program.cs
@@ -7,6 +7,7 @@ namespace Wgit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Changes Made");
         }
     }
 }
